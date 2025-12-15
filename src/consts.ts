c7f590f4cc9b94a5e6fbb7606a64fa7432497cd3
Emerald Zhu <zhuemerald@gmail.com>
2025-12-15 15:25:40 +0300

    Updated site config

--- src/consts.ts
+++ src/consts.ts
@@ -2,9 +2,9 @@ import type { Site, Page, Links, Socials } from "@types"
 
 // Global
 export const SITE: Site = {
-  TITLE: "Astro Sphere",
-  DESCRIPTION: "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  TITLE: "EMERALD ZHU",
+  DESCRIPTION: "Welcome to my small world. See my portfolio. Occasionally write blogs.",
+  AUTHOR: "Emerald Zhu",
 }
 
 // Work Page
@@ -56,26 +56,26 @@ export const SOCIALS: Socials = [
   { 
     NAME: "Email",
     ICON: "email", 
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
+    TEXT: "hello@emeraldzhu.com",
+    HREF: "mailto:hello@emeraldzhu.com",
   },
   { 
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere"
+    TEXT: "emeraldzhu",
+    HREF: "https://github.com/emeraldzhu"
   },
   { 
     NAME: "LinkedIn",
     ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+    TEXT: "justus-nzioki",
+    HREF: "https://www.linkedin.com/in/justusnzioki/",
   },
   { 
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "emeraldzhu",
+    HREF: "https://x.com/EmeraldZhu",
   },
 ]
 
